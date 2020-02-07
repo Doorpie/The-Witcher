39fd628434ec5b590fef7620401dcc19e127744f
Andrei <Knclav@gmail.com>
2020-02-07 22:31:02 +0200

    update css

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.series__arrow',
       },
+    breakpoints: {
+        575: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.header');
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('menu-btn--active');
+    menu.classList.toggle('header--active');
 });
 
